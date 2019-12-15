c25045f33ce3236ad71c4017f25ef45387b3f116
Wesley Belleman <wesley.belleman.2@us.af.mil>
2019-12-15 08:00:51 -0800

    Added gitignore entries

--- read_html.py
+++ read_html.py
@@ -1,11 +1,19 @@
 import os
 
-sequence = 0
+letter_order = "абвгґдеєжзиіїйклмнопрстуфхцчшщьюя"
+cap_letter_order = "АБВГҐДЕЄЖЗИІЇЙКЛМНОПРСТУФХЦЧШЩЬЮЯ"
+
+low_sequence = 0
+cap_sequence = 0
 for x in os.listdir('html_files'):
 	if x[0:4] == 'html':
 		f_origin_file = open('html_files/'+x,'r')
 		f_origin = f_origin_file.read()
-		if f_origin.find('<span class="word_style" >а') != -1:
-			f_destination = open('word_files/'+'а_'+str(sequence),'w+')
+		if f_origin.find('<span class="word_style" >б') != -1:
+			f_destination = open('word_files/б/'+'б_'+str(low_sequence),'w+')
 			f_destination.write(f_origin)
-			sequence += 1
+			low_sequence += 1
+		if f_origin.find('<span class="word_style" >Б') != -1:
+			f_destination = open('word_files/б/'+'cap_б_'+str(cap_sequence),'w+')
+			f_destination.write(f_origin)
+			cap_sequence += 1
